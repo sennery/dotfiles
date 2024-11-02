356c7835d11a7eadb5ef98d161124ced7690a403
Arsenii Likhachev <mac452ares@gmail.com>
2024-11-02 23:26:40 +0500

    feat(nvim): add JSON and YAML schema support

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -17,6 +17,9 @@ return {
 
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
+
+      -- JSON and YAML schema support
+      'b0o/schemastore.nvim',
     },
     config = function()
       -- vim.lsp.set_log_level 'debug'
@@ -132,6 +135,28 @@ return {
         },
 
         bashls = {},
+        jsonls = {
+          settings = {
+            json = {
+              schemas = require('schemastore').json.schemas(),
+              validate = { enable = true },
+            },
+          },
+        },
+        yamlls = {
+          settings = {
+            yaml = {
+              schemaStore = {
+                -- You must disable built-in schemaStore support if you want to use
+                -- this plugin and its advanced options like `ignore`.
+                enable = false,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = '',
+              },
+              schemas = require('schemastore').yaml.schemas(),
+            },
+          },
+        },
       }
 
       local ensure_installed = vim.tbl_keys(servers or {})
