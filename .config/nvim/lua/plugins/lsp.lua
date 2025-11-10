707ee1a947eff102378d740f8d5e4b7c2d3e51b9
Arsenii Likhachev <mac452ares@gmail.com>
2025-11-10 12:25:41 +0500

    feat(nvim): update vtsls lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -60,11 +60,11 @@ return {
             },
             typescript = {
               inlayHints = {
-                includeInlayVariableTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayParameterNameHints = 'literals', -- 'none' | 'literals' | 'all'
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
+                variableTypes = true,
+                functionLikeReturnTypeHints = true,
+                parameterNameHints = 'literals', -- 'none' | 'literals' | 'all'
+                functionParameterTypeHints = true,
+                propertyDeclarationTypeHints = true,
               },
             },
           },
@@ -83,31 +83,6 @@ return {
               },
             },
           },
-          on_init = function(client)
-            client.handlers['tsserver/request'] = function(_, result, context)
-              local clients = vim.lsp.get_clients { bufnr = context.bufnr, name = 'vtsls' }
-              if #clients == 0 then
-                vim.notify('Could not find `vtsls` lsp client, `vue_ls` would not work without it.', vim.log.levels.ERROR)
-                return
-              end
-              local ts_client = clients[1]
-
-              local param = unpack(result)
-              local id, command, payload = unpack(param)
-              ts_client:exec_cmd({
-                title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-                command = 'typescript.tsserverRequest',
-                arguments = {
-                  command,
-                  payload,
-                },
-              }, { bufnr = context.bufnr }, function(_, r)
-                local response_data = { { id, r.body } }
-                ---@diagnostic disable-next-line: param-type-mismatch
-                client:notify('tsserver/response', response_data)
-              end)
-            end
-          end,
         },
 
         cssls = {},
