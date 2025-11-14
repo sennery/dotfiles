6dbd05dabe0d0c4603e9e989cc8a237cf62b7a19
Arsenii Likhachev <mac452ares@gmail.com>
2025-11-14 10:04:48 +0500

    feat(nvim): add frecency telescope keymap

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -105,9 +105,11 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
-      vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = '[ ] Search Files' })
       vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
       vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = '[G]it [B]ranches' })
+      vim.keymap.set('n', '<leader><leader>', function()
+        vim.cmd ':Telescope frecency workspace=CWD'
+      end, { desc = '[G]it [B]ranches' })
 
       vim.keymap.set('n', '<leader>sf', function()
         builtin.find_files {
